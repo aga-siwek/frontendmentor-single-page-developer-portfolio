1f96106bbca6fd1c889ab722ac9ac4c04efe1613
Aga Siwek <176235224+aga-siwek@users.noreply.github.com>
2025-05-15 20:19:50 +0200

    add role

--- src/components/Experiances/Experiances.jsx
+++ src/components/Experiances/Experiances.jsx
@@ -4,7 +4,7 @@ import ExperianceBox from "./ExperianceBox";
 function Experiances() {
 
     return (
-        <div className={styles.experiances}>
+        <div className={styles.experiances} role="contentinfo">
             <hr className={styles.experiances__line}/>
             <div className={styles.experiances_boxs}>
                 <ExperianceBox kind="HTML" experianceYears={4} />
